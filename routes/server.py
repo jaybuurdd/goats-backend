87e33a6d09a36dd5bbae8fd8dbe0a120585706b8
jas <job.t.developer@gmail.com>
2024-03-02 19:23:56 -0500

    updated health check endpoint

--- routes/server.py
+++ routes/server.py
@@ -3,7 +3,5 @@ from fastapi import APIRouter, Response
 router = APIRouter()
 
 @router.get("/health-check")
-async def ping(response: Response):
-    response.status_code = 200
-    response.body = {"status": "OK"}
-    return response
+async def ping():
+    return {"status": "OK"}
