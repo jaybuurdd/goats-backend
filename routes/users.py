4d55f2b5b6b44a74a2144be82d4e68ddc97e6b54
jas <job.t.developer@gmail.com>
2024-05-20 00:02:49 -0400

    possible resolution force exposing set-cookie header for mobile browser cookie issue

--- routes/users.py
+++ routes/users.py
@@ -40,7 +40,7 @@ async def auth( response: Response, data: dict, db: Session = Depends(get_db)):
         value=token, 
         httponly=True, 
         samesite='None', 
-        secure=False, # if os.getenv("APP_MODE", "DEVELOPMENT").upper() == 'DEVELOPMENT' else True,  #NOTE: secure=False for local testing
+        secure=False if os.getenv("APP_MODE", "DEVELOPMENT").upper() == 'DEVELOPMENT' else True,  #NOTE: secure=False for local testing
         # domain="api-goatsdao.onrender.com",
         path='/'
     )
