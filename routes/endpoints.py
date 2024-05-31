71796dd6aab9fef9e5ebd7e96d304969c04c47aa
jas <job.t.developer@gmail.com>
2024-05-30 21:20:31 -0400

    added option handler for pre-flight requests

--- routes/endpoints.py
+++ routes/endpoints.py
@@ -1,5 +1,6 @@
 import os
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 from utils.logging import logger, LoggingMiddleware
@@ -17,7 +18,6 @@ def register_endpoints():
         # 'http://localhost:3000'
     ]
 
-    api.add_middleware(LoggingMiddleware)
     api.add_middleware(
         CORSMiddleware,
         allow_origins=origins,
@@ -30,5 +30,16 @@ def register_endpoints():
     api.include_router(user_router, prefix="/api/v1/user", tags=["users"])
     api.include_router(wallet_router, prefix="/api/v1/wallet", tags=["wallets"])
     api.include_router(server_router, prefix="/api/v1/server", tags=["servers"])
+    
+    
+    @api.options("/{path:path}")
+    async def option_handler():
+        return JSONResponse(content={"message": "OK"}, headers={
+            "Access-Control-Allow-Origin": "https://goatsdao.com",
+            "Access-Control-Allow-Methods": "OPTIONS, GET, POST, PUT, DELETE",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization, Cookie, Set-Cookie",
+            "Access-Control-Allow-Credentials": "true"
+        })
+    
     logger.info("Ending API endpoints registration")
     return api
